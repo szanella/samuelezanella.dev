d6138c958418d2777e285c023c65cfe65972f0a2
Samuele Zanella <samuele@moku.io>
2019-05-07 15:10:48 +0200

    Initial laying around with clip-paths

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      containerState: 1
+    };
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      const timestamp = (new Date()).getTime();
+      const nextContainerState = this.state.containerState === 2 ? 1 : this.state.containerState + 1;
+
+      this.setState({
+        containerState: nextContainerState
+      });
+    }, 1500);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <div className={`shards-container state-${this.state.containerState}`}>
+          <div className='shard-wrap'>
+            <div className='shard'></div>
+          </div>
+          <div className='shard-wrap'>
+            <div className='shard'></div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+};
 
 export default App;
