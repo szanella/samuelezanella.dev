235be51195271c213ff83bb58bfbe73ac89e2850
Samuele Zanella <samuele@moku.io>
2019-05-07 20:15:18 +0200

    First animation prototypes

--- src/App.js
+++ src/App.js
@@ -9,27 +9,39 @@ class App extends React.Component {
     this.state = {
       containerState: 1
     };
+
+    this.toNextState = this.toNextState.bind(this);
   }
 
   componentDidMount() {
-    this.interval = setInterval(() => {
-      const timestamp = (new Date()).getTime();
-      const nextContainerState = this.state.containerState === 2 ? 1 : this.state.containerState + 1;
+    // this.interval = setInterval(this.toNextState, 1500);
+  }
+
+  componentWillUnmount() {
+    // clearInterval(this.interval);
+  }
+
+  toNextState() {
+    const nextContainerState = this.state.containerState === 2 ? 1 : this.state.containerState + 1;
 
       this.setState({
         containerState: nextContainerState
       });
-    }, 1500);
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.interval);
   }
 
   render() {
     return (
       <div className="app">
-        <div className={`shards-container state-${this.state.containerState}`}>
+        <div className={`shards-container state-${this.state.containerState}`} onClick={this.toNextState}>
+          <div className='shard-wrap'>
+            <div className='shard'></div>
+          </div>
+          <div className='shard-wrap'>
+            <div className='shard'></div>
+          </div>
+          <div className='shard-wrap'>
+            <div className='shard'></div>
+          </div>
           <div className='shard-wrap'>
             <div className='shard'></div>
           </div>
